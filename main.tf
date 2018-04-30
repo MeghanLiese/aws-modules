28a604d8ac14ceb9da176f182b582df0817ac1b0
MeghanLiese <meghan@hashicorp.com>
2018-04-29 22:55:25 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,9 @@ provider "aws" {
 resource "aws_vpc" "demo_vpc" {
   cidr_block = "${var.vpc_cidr_block}"
 
-  tags {
-    Name = "fp_demo_subnet"
+   tags = {
+    Terraform = "true"
+    Environment = "dev"
   }
 }
 
@@ -15,7 +16,8 @@ resource "aws_subnet" "demo_subnet" {
   cidr_block        = "${var.subnet_cidr_block}"
   availability_zone = "${var.subnet_availability_zone}"
 
-  tags {
-    Name = "fp_demo_subnet"
+   tags = {
+    Terraform = "true"
+    Environment = "dev"
   }
 }
